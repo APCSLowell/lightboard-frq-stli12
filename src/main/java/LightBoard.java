690e6159718d9721c1364601333a0bc7f1c0f663
stli12 <143028001+stli12@users.noreply.github.com>
2024-04-29 21:15:25 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -11,13 +11,10 @@ public class LightBoard
   public LightBoard(int numRows, int numCols)
   {
     lights = new boolean[numRows][numCols];
-    for(int i =0; i < lights.length; i++){
-      for(int j = 0; j < lights[0].length; j++){
+    for(int r =0; r < lights.length; r++)
+      for(int c = 0; c < lights[0].length; c++)
         if(Math.random() <= 0.4)
-          lights[i][j] = true;
-      }
-    }
-
+          lights[r][c] = true;
   }
 
   /** Evaluates a light in row index row and column index col and returns a status
@@ -27,15 +24,19 @@ public class LightBoard
   public boolean evaluateLight(int row, int col)
   {
      int count = 0;
-    for(int i =0; i < lights.length; i ++){
-      if(lights[i][col] == true)
+    for(int r = 0; r < lights.length; r ++){
+      if(lights[r][col])
         count++;
     }
 
-    if(lights[row][col] == true && count % 2==0)
-      return false;
-    if(lights[row][col] == false && count % 3 == 0)
-      return true;
+    if(lights[row][col]){
+      if(count % 2 ==0)
+        return false;
+    }
+      else{
+        if(count % 3 == 0)
+          return true;
+      }
 
       return lights[row][col];
   }
